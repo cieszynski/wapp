7a0e6a744f00c9dfbbd9d4328cf40a1fba31b48e
cieszynski <admin@cieszynski.de>
2023-03-17 00:38:35 +0100

    loading fonts, insert css once

--- wapp.mjs
+++ wapp.mjs
@@ -8,7 +8,9 @@ const
 
 const
     css_compact = new CSSStyleSheet(),
-    css_default = new CSSStyleSheet();
+    css_default = new CSSStyleSheet(),
+    css_medium = new CSSStyleSheet(),
+    css_expanded = new CSSStyleSheet();
 
 export const navigationItem = (properties) => {
     const elem = Object.create(null, {
@@ -17,6 +19,8 @@ export const navigationItem = (properties) => {
                 const label = document.createElement('label');
                 const input = document.createElement('input');
                 const span = document.createElement('span');
+                input.dataset.icon = '\ue88a';
+                label.className = 'navigation';
                 label.append(input, span);
                 input.name = 'navigation';
                 input.type = 'radio';
@@ -35,6 +39,81 @@ export const navigationItem = (properties) => {
 
     Object.assign(elem, properties);
     Object.freeze(elem);
+
+    if (!navigationItem.seen) {
+        navigationItem.seen = true;
+
+        css_default.insertRule(`
+            label.navigation {
+                font-family: Roboto-Medium;
+                font-weight: 500;
+                font-size: 12rem;
+                line-height: 1;
+                position: relative;
+                display: block;
+                padding: 36rem 0 0 0;
+                text-align: center;
+            }
+        `)
+
+        css_default.insertRule(`
+            label.navigation input {
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: -1;
+                appearance: none;
+                position: absolute;
+            }
+            `)
+
+        css_default.insertRule(`
+        label.navigation input::after {
+            content: attr(data-icon);
+            font-family: Material-Icons-Outlined;
+            font-weight: 500;
+            font-size: 24rem;
+            line-height: 1.3;
+        }
+            `)
+
+        css_default.insertRule(`
+        label.navigation input:checked::after {
+            font-family: Material-Icons;
+        }
+        `);
+
+        css_compact.insertRule(`
+        label.navigation {
+            width: 100%;
+            height: 100%;
+        }
+        `);
+
+        css_compact.insertRule(`
+        label.navigation input::before {
+            content: "";
+            position: relative;
+            border-radius: 16rem;
+            display: block;
+            height: 32rem;
+            width: 64rem;
+            margin: auto;
+        }
+        `);
+
+        css_compact.insertRule(`
+        label.navigation input::after {
+            position: relative;
+            width: 100%;
+            margin: auto;
+            text-align: center;
+            position: absolute;
+            top: 0;
+        }
+        `);
+    }
 
     return elem;
 }
@@ -66,19 +145,67 @@ export const navigationBar = (properties) => {
     Object.assign(elem, properties);
     Object.freeze(elem);
 
-    elem.node.addEventListener('click', function (e) {
-        for (const child of this.children) {
-            console.log(child)
+    if (!navigationBar.seen) {
+        navigationBar.seen = true;
+
+        css_default.insertRule(`
+            nav { 
+                z-index: 0;
+                display: flex;
+                background-color: rgba(0,0,0,.01); 
+                height: 80px;
+            }
+        `);
+
+        css_compact.insertRule(`
+        nav {   /* BOTTOM NAVIGATION */
+            justify-content: center;
+            column-gap: 8rem;
+            width: 100%;
+            height: 80rem;
+            padding: 12rem 0 16rem 0;
         }
-    })
+        `)
+    }
 
     return elem;
 }
 
-export const application = (properties) => {
+export const theme = (properties) => {
+    const elem = Object.create(null, {
+        regularFont: {
+            value: new FontFace("Roboto-Regular", "url(Roboto-Regular.ttf)"),
+            writable: true
+        },
+        mediumFont: {
+            value: new FontFace("Roboto-Medium", "url(Roboto-Medium.ttf)"),
+            writable: true
+        },
+        iconFont: {
+            value: new FontFace("Material-Icons", "url(MaterialIcons-Regular.ttf)"),
+            writable: true
+        },
+        iconOutlinedFont: {
+            value: new FontFace("Material-Icons-Outlined", "url(MaterialIconsOutlined-Regular.otf)"),
+            writable: true
+        }
+    });
+
+    Object.assign(elem, properties);
+    Object.freeze(elem);
+
+    return elem;
+}
+
+export const application = (properties, defaultTheme = theme()) => {
+    document.fonts.add(defaultTheme.regularFont);
+    document.fonts.add(defaultTheme.mediumFont);
+    document.fonts.add(defaultTheme.iconFont);
+    document.fonts.add(defaultTheme.iconOutlinedFont);
+
     const elem = Object.create(null);
 
-    return new Promise(resolve => {
+    const contentLoaded = new Promise(resolve => {
         document.addEventListener("DOMContentLoaded", (event) => {
             css_default.insertRule(`
             *,
@@ -89,14 +216,22 @@ export const application = (properties) => {
                 user-select: none;
             }`);
 
+            css_default.insertRule(`
+            html {
+                height: 100%;
+                font-size: 6.25%;
+                font-family: Roboto-Regular;
+            }`)
+
             css_compact.insertRule(`
-                @media screen and (max-width: 599px) {
-                    body { background-color: #ddd;}
-                }
-            `);
+            @media screen and (max-width: 599px) {
+                body { background-color: #ddd;}
+            }`);
 
             document.adoptedStyleSheets[CSS_DEFAULT] = css_default;
             document.adoptedStyleSheets[CSS_COMPACT] = css_compact;
+            document.adoptedStyleSheets[CSS_MEDIUM] = css_medium;
+            document.adoptedStyleSheets[CSS_EXPANDED] = css_expanded;
 
             Object.defineProperties(elem, {
                 node: {
@@ -110,9 +245,6 @@ export const application = (properties) => {
                     set(other) {
                         const old = document.querySelector('nav');
                         if (!old) {
-                            css_default.insertRule(`
-                                nav { background-color: red; height: 80px; }
-                            `);
                             document.body.appendChild(other.node);
                         } else {
                             document.body.replaceChild(other.node, old);
@@ -127,4 +259,12 @@ export const application = (properties) => {
             resolve(elem);
         });
     });
+
+    return Promise.all([
+        defaultTheme.regularFont.load,
+        defaultTheme.mediumFont.load,
+        defaultTheme.iconFont.load,
+        defaultTheme.iconOutlinedFont.load,
+        contentLoaded
+    ]);
 }
